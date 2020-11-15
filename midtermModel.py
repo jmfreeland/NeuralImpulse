b026121ed7cd5a31d040323dbfecaea60d17f3b4
jmfreeland <josh.freeland@gmail.com>
2020-11-15 14:15:13 +0000

    multiple updates

--- midtermModel.py
+++ midtermModel.py
@@ -15,8 +15,11 @@ import numpy as np
 import seaborn as sns
 import soundfile as sf
 
-from keras import backend as K
+from tensorflow.keras import backend as K
 import gc
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
@@ -25,13 +28,16 @@ print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('
 K.clear_session()
 gc.collect()
 
-test_audio = nt.neuralTransform('C:/users/freel/Desktop/neuralImpulse/input_unprocessed/SL68Trial_Raw.aif',
-                            'C:/users/freel/Desktop/neuralImpulse/input_processed/SL68Trial.aif',
-                            96000,
+sample_rt = 48000
+lookback = 2000
+
+test_audio = nt.neuralTransform('C:/users/freel/Desktop/neuralImpulse/input_unprocessed/unprocessed_full_Nov1.wav',
+                            'C:/users/freel/Desktop/neuralImpulse/input_processed/processed_full_Nov1.wav',
+                            sample_rt,
                             learning_rt=.001,
-                            epoch_count=3,
-                            batch_sz=2048,
-                            dropout_rate=.1)
+                            epoch_count=20,
+                            batch_sz=1024,
+                            dropout_rate=.05)
 
 #start by graphing the input signal and output signal for reference
 fig = plt.figure(dpi=300, figsize=(9,6))
@@ -48,7 +54,7 @@ test_audio.fit_linear()
 test_audio.linear_model.summary()
 linear_output = test_audio.transform_linear()
 fig = plt.figure(dpi=300, figsize=(9,6))
-librosa.display.waveplot(y=linear_output[:,0], sr=96000)
+librosa.display.waveplot(y=linear_output[:,0], sr=sample_rt)
 fig.suptitle('SL68 through pure one-step linear model')
 test_audio.visualize_linear()
 
@@ -63,10 +69,10 @@ plt.plot(linear_diff)
 fig = plt.figure(dpi=300, figsize=(9,6))
 fig.suptitle('Correlation between input and output in original signal')
 sns.regplot(x=test_audio.input_clip[0][0:10000], y=test_audio.output_clip[0][0:10000])
-sf.write('C:/users/freel/Desktop/neuralImpulse/output/SL68_linear.wav', linear_output, 96000)
+sf.write('C:/users/freel/Desktop/neuralImpulse/output/SL68_linear.wav', linear_output, sample_rt)
 #fit a linear regression with scikit and compare
 
-print('\nfitting linear multi-step model with 1000 sample lookback')
+print('\nfitting linear multi-step model with' + ' {}'.format(lookback) + ' sample lookback')
 test_audio.fit_linear_multi(1000)
 test_audio.linear_multi_model.summary()
 multi_step_weights = test_audio.linear_multi_model.get_layer(index=0).get_weights()
@@ -76,15 +82,15 @@ sns.lineplot(data=multi_step_weights[0])
 linear_multi_output = test_audio.transform_linear_multi()
 fig=plt.figure(dpi=300, figsize=(9,6))
 fig.suptitle('multi-step linear model output')
-librosa.display.waveplot(y=linear_multi_output[:,0], sr=96000)
+librosa.display.waveplot(y=linear_multi_output[:,0], sr=sample_rt)
 
 linear_multi_diff = np.transpose(test_audio.output_clip[0])[1000:] - linear_multi_output[:,0]
 fig = plt.figure(dpi=300, figsize=(9,6))
 fig.suptitle('difference with linear multi-step model')
-librosa.display.waveplot(y=linear_multi_diff, sr=96000)
+librosa.display.waveplot(y=linear_multi_diff, sr=sample_rt)
 
 #output to audio file
-sf.write('C:/users/freel/Desktop/neuralImpulse/output/SL68_linear_multistep_1000.wav', linear_multi_output, 96000)
+sf.write('C:/users/freel/Desktop/neuralImpulse/output/SL68_linear_multistep_1000.wav', linear_multi_output, sample_rt)
 
 print('\nfitting double-layer model with 1000 sample lookback')
 #test dense double layer model
@@ -95,8 +101,8 @@ dense2_model_output = test_audio.transform_dense2()
 #how does two-layer dense model look?
 fig=plt.figure(dpi=300, figsize=(9,6))
 fig.suptitle('double-layer dense model output')
-librosa.display.waveplot(y=dense2_model_output[:,0], sr=96000)
-sf.write('C:/users/freel/Desktop/neuralImpulse/output/SL68_dense2.wav', dense2_model_output, 96000)
+librosa.display.waveplot(y=dense2_model_output[:,0], sr=sample_rt)
+sf.write('C:/users/freel/Desktop/neuralImpulse/output/SL68_dense2.wav', dense2_model_output, sample_rt)
 
 #test three layer model with rnn after first stage
 test_audio.fit_rnn_dense3(128,64,32,16)
@@ -105,8 +111,8 @@ rnn_dense3_model_output = test_audio.transform_rnn_dense3()
 
 fig=plt.figure(dpi=300, figsize=(9,6))
 fig.suptitle('early rnn, triple-layer dense model output')
-librosa.display.waveplot(y=rnn_dense3_model_output[:,0], sr=96000)
-sf.write('C:/users/freel/Desktop/neuralImpulse/output/SL68_rnn_dense3.wav', rnn_dense3_model_output, 96000)
+librosa.display.waveplot(y=rnn_dense3_model_output[:,0], sr=sample_rt)
+sf.write('C:/users/freel/Desktop/neuralImpulse/output/SL68_rnn_dense3.wav', rnn_dense3_model_output, sample_rt)
 
 #test three layer model with lstm after first stage
 test_audio.fit_lstm_dense3(128,64,32,16)
@@ -115,8 +121,8 @@ lstm_dense3_model_output = test_audio.transform_lstm_dense3()
 
 fig=plt.figure(dpi=300, figsize=(9,6))
 fig.suptitle('early lstm, triple-layer dense model output')
-librosa.display.waveplot(y=lstm_dense3_model_output[:,0], sr=96000)
-sf.write('C:/users/freel/Desktop/neuralImpulse/output/SL68_lstm_dense3.wav', lstm_dense3_model_output, 96000)
+librosa.display.waveplot(y=lstm_dense3_model_output[:,0], sr=sample_rt)
+sf.write('C:/users/freel/Desktop/neuralImpulse/output/SL68_lstm_dense3.wav', lstm_dense3_model_output, sample_rt)
 
 #show the training history
 test_audio.plot_errors()
---
+++ saveModels.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 14:36:32 2020
+
+@author: freel
+"""
+
+test_audio.linear_model.save_weights('./checkpoints/linear_model')
+test_audio.linear_multi_model.save_weights('./checkpoints/linear_multi_model')
+test_audio.dense2_model.save_weights('./checkpoints/dense2_model')
+test_audio.lstm_dense3_model.save_weights('./checkpoints/lstm_dense3_model')
+test_audio.rnn_dense3_model.save_weights('./checkpoints/rnn_dense3_model')
+
+
+
